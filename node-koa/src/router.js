b3221552e0b401106757f5b5a0d8d7caed4bcc01
lzw <lzwdot@qq.com>
2021-12-12 21:14:36 +0800

    update

--- node-koa/src/router.js
+++ node-koa/src/router.js
@@ -11,4 +11,35 @@ class Router {
   get(path, middleware) {
     this.register(path, 'get', middleware)
   }
+
+  post(path, middleware) {
+    this.register(path, 'post', middleware)
+  }
+
+  routes() {
+    let stock = this.stack
+    return async function (ctx, next) {
+      let currPath = ctx.url
+      let route
+
+      for (let i = 0; i < stock.length; i++) {
+        let item = stock[i]
+
+        console.log(currPath, ctx.method)
+
+        // 判断 path 和 method
+        if (currPath === item.path && item.methods.indexOf(ctx.method) >= 0) {
+          route = item.middleware
+          break
+        }
+      }
+
+      if (typeof route === 'function') {
+        route(ctx, next)
+        return
+      }
+    }
+  }
 }
+
+module.exports = Router
--- node-koa/src/test.js
+++ node-koa/src/test.js
@@ -3,27 +3,28 @@ const Router = require('./router')
 const app = new Koa()
 const router = new Router()
 
-route.get('/index', async ctx => {
+router.get('/index', async ctx => {
   ctx.body = 'index'
 })
-route.get('/post', async ctx => {
+router.get('/post', async ctx => {
   ctx.body = 'post'
 })
-route.get('/list', async ctx => {
+router.get('/list', async ctx => {
   ctx.body = 'list'
 })
-route.post('/index', async ctx => {
+router.post('/index', async ctx => {
   ctx.body = 'post index'
 })
+app.use(router.routes())
 
 // app.use((req,res)=>{
 //   res.writeHead(200)
 //   res.end('hi 666')
 // })
 
-app.use(ctx => {
-  ctx.body = 'hi 666'
-})
+// app.use(ctx => {
+//   ctx.body = 'hi 666'
+// })
 
 app.listen(8080, () => {
   console.log('start at 8080')
