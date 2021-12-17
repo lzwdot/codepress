3f0b8a62fb60038d8079065f7f91b72a5ad0b36b
lzw <lzwdot@qq.com>
2021-12-17 16:30:09 +0800

    init

---
+++ vue-src/example/src/kvue-form/Index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <k-input v-model="model.usernmae"></k-input>
+    {{model。username}}
+  </div>
+</template>
+<script>
+import KInput from "./KInput";
+export default {
+  components: {KInput},
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  method: {
+    onInput(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
---
+++ vue-src/example/src/kvue-form/KInput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="onInput">
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  method: {
+    onInput(e) {
+      this.$emit('input', e.target.value)
+    }
+  }
+}
+</script>
