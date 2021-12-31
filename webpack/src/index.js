fa010ac8333bec4a7ad97dfc649e2f7e86009073
lzw <lzwdot@qq.com>
2021-12-31 11:32:22 +0800

    update

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,6 +1,9 @@
 import less from './css/index.less'
 import axios from "axios";
 
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 // axios.get('/api/info').then(res => {
 //   console.log(res)
 // })
