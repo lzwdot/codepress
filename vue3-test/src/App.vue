81a1ec66c63ef36db69ca2a4f8029d4a6722adeb
lzw <lzwdot@qq.com>
2022-01-20 17:46:50 +0800

    update

---
+++ vue3-test/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <Ref/>
+  <toRef/>
+  <to-refs></to-refs>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+
+import Ref from "@/components/Ref";
+import ToRef from "@/components/ToRef";
+import ToRefs from "@/components/ToRefs";
+
+export default {
+  name: 'App',
+  components: {
+    ToRefs,
+    ToRef,
+    Ref
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue3-test/src/components/WhyRef.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+import {reactive, toRefs} from "vue";
+
+function useFeatureX() {
+  const state = reactive({
+    x: 1,
+    y: 2
+  })
+
+  return toRefs(state)
+}
+
+export default {
+  name: "WhyRef",
+  setup() {
+    const {x, y} = useFeatureX()
+  }
+}
+</script>
