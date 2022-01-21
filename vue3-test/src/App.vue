b18acdd370350d428573afcfc6e7ea5663122fc1
lzw <lzwdot@qq.com>
2022-01-21 17:33:00 +0800

    update

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,37 +1,58 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <Ref/>
-  <toRef/>
-  <to-refs></to-refs>
+  <!--  <Ref/>-->
+  <!--  <toRef/>-->
+  <!--  <to-refs></to-refs>-->
+  <!--  <why-ref></why-ref>-->
+  <!--  <why-value></why-value>-->
+  <!--  <mouse-position></mouse-position>-->
+  <p>{{ name }}{{ age }}</p>
+  <!--  <child-->
+  <!--      v-model:name="name"-->
+  <!--      v-model:age="age"-->
+  <!--  ></child>-->
+<!--  <watch></watch>-->
+  <get-instance></get-instance>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 
 
-import Ref from "@/components/Ref";
-import ToRef from "@/components/ToRef";
-import ToRefs from "@/components/ToRefs";
+// import Ref from "@/components/Ref";
+// import ToRef from "@/components/ToRef";
+// import ToRefs from "@/components/ToRefs";
+// import WhyRef from "@/components/WhyRef";
+// import WhyValue from "@/components/WhyValue";
+// import MousePosition from "@/components/MousePosition";
+
+// import Child from "@/components/Child";
+import {reactive, toRefs} from "vue";
+// import Watch from "@/components/Watch";
+import GetInstance from "@/components/GetInstance";
 
 export default {
   name: 'App',
   components: {
-    ToRefs,
-    ToRef,
-    Ref
+    GetInstance,
+    // Watch
+    // Child
+    // MousePosition,
+    // WhyValue,
+    // WhyRef,
+    // ToRefs,
+    // ToRef,
+    // Ref
     // HelloWorld
+  },
+  setup() {
+    const state = reactive({
+      name: 'lzw.',
+      age: '20'
+    })
+
+    return toRefs(state)
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
