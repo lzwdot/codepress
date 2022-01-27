ef33f66126d8f95d0cb154b220e13097cffddf06
lzw <lzwdot@qq.com>
2022-01-27 14:09:56 +0800

    update

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -4,6 +4,7 @@ import BaseUse from './components/baseUse'
 import AdvanceUse from "./components/advanceUse";
 import React from "react";
 import Immutable from "immutable";
+import ReduxDemo from './components/reduxUse'
 
 // import JSXBaseDemo from "./components/baseUse/JSXBaseDemo";
 
@@ -27,6 +28,7 @@ function App() {
       <BaseUse/>
       <AdvanceUse/>
       {/*<JSXBaseDemo/>*/}
+      <ReduxDemo />
     </div>
   );
 }
@@ -35,7 +37,6 @@ const map1 = Immutable.Map({a: 1, b: 2, c: 3})
 const map2 = map1.set('b', 50)
 console.log(map1.get('b')) // 2
 console.log(map2.get('b')) // 50
-
 
 
 export default App;
