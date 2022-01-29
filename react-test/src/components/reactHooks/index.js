673b00fb52b4f6e42262704a528d4f7002f0388a
lzw <lzwdot@qq.com>
2022-01-29 11:31:58 +0800

    hooks

--- react-test/src/components/reactHooks/index.js
+++ react-test/src/components/reactHooks/index.js
@@ -1,5 +1,10 @@
 import React, {useState} from "react";
 import FriendStatus from "./FriendStatusClass";
+import UseRefDemo from "./UseRefDemo";
+import UseContextDemo from "./UseContextDemo";
+import UseReducerDemo from './UseReducerDemo'
+import UseMemoDemo from "./UseMemoDemo";
+import UseCallbackDemo from "./UseCallbackDemo";
 
 function ReactHooks() {
   const [flag, setFlag] = useState(true)
@@ -8,7 +13,12 @@ function ReactHooks() {
   return <div>
     <button onClick={() => setFlag(false)}>设置 flag</button>
     <button onClick={() => setId(id + 1)}>设置 ID</button>
-    {flag && <FriendStatus friendId={id}></FriendStatus>}
+    {/*{flag && <FriendStatus friendId={id}></FriendStatus>}*/}
+    {/*<UseRefDemo></UseRefDemo>*/}
+    {/*<UseContextDemo></UseContextDemo>*/}
+    {/*<UseReducerDemo></UseReducerDemo>*/}
+    {/*<UseMemoDemo></UseMemoDemo>*/}
+    <UseCallbackDemo></UseCallbackDemo>
   </div>
 }
 
